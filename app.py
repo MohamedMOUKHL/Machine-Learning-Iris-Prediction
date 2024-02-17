407c067d6b5ec7dfdc9c90d59cd0a794b2b18acd
mohamed <mohamed.moukhlissi2-etu@etu.univh2c.ma>
2024-02-17 22:00:12 +0100

    2nd commit

--- app.py
+++ app.py
@@ -1,27 +1,46 @@
-from flask import Flask, render_template, request, jsonify
-from flask_restful import Resource, Api
+from flask import Flask, request, render_template
+from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier
-import pickle
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 app = Flask(__name__)
-api = Api(app)
 
-# Charger le modèle
-with open('knn_model.pkl', 'rb') as model_file:
-    knn = pickle.load(model_file)
+# Load the dataset and train the model
+iris_dataset = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train, y_train)
 
-class PredictionResource(Resource):
-    def post(self):
-        data = request.get_json()
-        nouvelles_mesures = [[data['longueur_sepale'], data['largeur_sepale'], data['longueur_petale'], data['largeur_petale']]]
-        prediction = knn.predict(nouvelles_mesures)
-        return {'prediction': int(prediction[0])}
-
-api.add_resource(PredictionResource, '/predict')
 
 @app.route('/')
-def index():
-    return render_template('index_api.html')
+def home():
+    return render_template('index.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    try:
+        # Get the data from the form
+        longueur_sepale = float(request.form['longueur_sepale'])
+        largeur_sepale = float(request.form['largeur_sepale'])
+        longueur_petale = float(request.form['longueur_petale'])
+        largeur_petale = float(request.form['largeur_petale'])
+
+        # Validate the input values
+        if not (0 <= longueur_sepale <= 10 and 0 <= largeur_sepale <= 10 and 0 <= longueur_petale <= 10 and 0 <= largeur_petale <= 10):
+            raise ValueError("Les mesures doivent être comprises entre 0 et 10.")
+
+        # Make the prediction
+        nouvelles_mesures = np.array([[longueur_sepale, largeur_sepale, longueur_petale, largeur_petale]])
+        predictions = knn.predict(nouvelles_mesures)
+        prediction_proba = knn.predict_proba(nouvelles_mesures)
+
+        # Render the result template with the prediction and probabilities
+        return render_template('result.html', prediction=predictions[0], proba_setosa=prediction_proba[0][0], proba_versicolor=prediction_proba[0][1], proba_virginica=prediction_proba[0][2])
+
+    except ValueError as e:
+        # Handle validation errors
+        return render_template('result.html', error_message=str(e))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="localhost", debug=True)
